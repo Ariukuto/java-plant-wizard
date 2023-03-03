69e469a126bcd8bbd23cfe8487a2de00b0bc86f5
Sven Büttner <68502281+Ariukuto@users.noreply.github.com>
2023-03-03 02:21:17 +0100

    Felder, Attribute in englische übersetzt

--- Plant.java
+++ Plant.java
@@ -1,46 +1,46 @@
  class Plant {
     public String name;
-    public String art;
-    public String winterHaerte;
-    public String pflege;
-    public String bluetenfarbe;
-    public String laubfarbe;
+    public String type;
+    public String winterHardinessZone;
+    public String careLevel;
+    public String flowerColors;
+    public String foliageColors;
     public String exposition;
-    public String laubphase;
+    public String foliagePhase;
     
     // Konstruktur
     Plant(
         String name, 
-        String art, 
-        String winterHaerte, 
-        String pflege, 
-        String bluetenfarbe, 
-        String laubfarbe, 
+        String type, 
+        String winterHardinessZone, 
+        String careLevel, 
+        String flowerColors, 
+        String foliageColors, 
         String exposition, 
-        String laubphase
+        String foliagePhase
     ) {
         // Hier werden die Werte, die über den Konstruktur übergeben werden, an die Felder weitergeben.
         this.name = name;
-        this.art = art;
-        this.winterHaerte = winterHaerte;
-        this.pflege = pflege;
-        this.bluetenfarbe = bluetenfarbe;
-        this.laubfarbe = laubfarbe;
+        this.type = type;
+        this.winterHardinessZone = winterHardinessZone;
+        this.careLevel = careLevel;
+        this.flowerColors = flowerColors;
+        this.foliageColors = foliageColors;
         this.exposition = exposition;
-        this.laubphase = laubphase;
+        this.foliagePhase = foliagePhase;
     }
     
     public String getInformation() {
         return 
         "{ \n"+ 
         "   name: " + this.name + "\n"
-        + "   art: " + this.art + "\n" 
-        + "   winterhärte: " + this.winterHaerte + "\n" 
-        + "   pflege: " + this.pflege + "\n" 
-        + "   blütenfarbe: " + this.bluetenfarbe + "\n" 
-        + "   laubfarbe: " + this.laubfarbe + "\n" 
+        + "   type: " + this.type + "\n" 
+        + "   winterhärte: " + this.winterHardinessZone + "\n" 
+        + "   careLevel: " + this.careLevel + "\n" 
+        + "   blütenfarbe: " + this.flowerColors + "\n" 
+        + "   foliageColors: " + this.foliageColors + "\n" 
         + "   exposition: " + this.exposition + "\n" 
-        + "   laubphase: " + this.laubphase + "\n"
+        + "   foliagePhase: " + this.foliagePhase + "\n"
         + "}";
     }
 }
