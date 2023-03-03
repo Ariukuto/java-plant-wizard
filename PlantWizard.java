4b42a277c0dee5ee52f7145981e9c741bc4608ce
Sven Büttner <68502281+Ariukuto@users.noreply.github.com>
2023-03-03 01:43:14 +0100

    Update PlantWizard.java

--- PlantWizard.java
+++ PlantWizard.java
@@ -1,1 +1,53 @@
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * @author: Sven Büttner
+ * @web: https://buettner-sven.de/
+ * @date: 02.02.2023
+ */
+public class PlantWizard
+{   
+    // Liste alle Pflanzen
+    private static ArrayList<Plant> plants = new ArrayList<Plant>();
+    
+    /**
+     * Wird als erstes ausgeführt 
+     */
+    public static void main(String[] args) 
+    {
+  
+        // Holt alle Pflanzen
+        getPlantsFromDatabase();
+        // Führt alle Fragen an den Benutzer aus
+        Questions.execute();
+        // Zeigt alle Antworten des Nutzers
+        Answers.show();
+
+        System.out.println(" Bitte beenden sie das Programm mit der Eingabe-Taste.");
+        EinAus.readInt();
+    }
+    
+    /**
+     * Soll Datenbankabfrage faken 
+     */
+    private static void getPlantsFromDatabase() 
+    {
+        plants.add(new Plant("Ceratodon Purpureus", "Moos", "6", "gering", "gruen braun rosa", "gruen braun", "sonnig halbschattig", "immergruen"));
+        plants.add(new Plant("Ceratodon Purpureus", "Moos", "6", "gering", "gruen braun rosa", "gruen braun", "sonnig halbschattig", "immergruen"));
+        plants.add(new Plant("Ceratodon Purpureus", "Moos", "6", "gering", "gruen braun rosa", "gruen braun", "sonnig halbschattig", "immergruen"));
+        plants.add(new Plant("Ceratodon Purpureus", "asfd", "6", "gering", "gruen braun rosa", "gruen braun", "sonnig halbschattig", "immergruen"));
+        plants.add(new Plant("Ceratodon Purpureus", "Moos", "6", "gering", "gruen braun rosa", "gruen braun", "sonnig halbschattig", "immergruen"));
+        plants.add(new Plant("Ceratodon Purpureus", "Moos", "6", "gering", "gruen braun rosa", "gruen braun", "sonnig halbschattig", "immergruen"));
+        plants.add(new Plant("Ceratodon Purpureus", "Moos", "6", "gering", "gruen braun rosa", "gruen braun", "sonnig halbschattig", "immergruen"));
+    }
+    
+    public void filter() {
+   
+    }
+    
+// Ende
+}
